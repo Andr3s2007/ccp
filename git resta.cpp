320ce70c0dba902465a56586866160da31ec92f0
sebas0gl <sebastiangutierrezlopez2007@gmail.com>
2025-05-17 14:42:30 -0600

    Add files via upload

---
+++ git resta.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+cout << "          resta" << endl; 
+cout << "-----------------------------" << endl;
+int num1, num2;
+
+cout << "ingresa el primer numero: ";
+cin>> num1;
+cout << "ingresa el segundo numero: ";
+cin>> num2;
+
+cout << "Resultado: " << num1 - num2 << endl;
+
+return 0;
+}
