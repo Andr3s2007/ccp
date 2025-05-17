fde003e2217d7b1ee5a4a3a3ca4e980b63a481ac
calacraft28 <127696718+calacraft28@users.noreply.github.com>
2025-05-17 15:22:00 -0600

    Add files via upload
    Una multiplicación bien perrona

---
+++ multiplicacion.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+
+using namespace std;    
+
+int main(){
+    float numero1, numero2;
+    cout << "multiplicacion de 2 digitos" << endl;
+    cout << "ingrese primer numero a multiplicar: " << endl;
+    cin >> numero1;
+    cout << "ingrese segundo numero a multiplicar: " << endl;
+    cin >> numero2;
+
+  float resultado = (numero1 + numero2);
+
+    cout << "resultado de su multiplicacion:" << resultado << endl;
+
+    return 0;
+
+}
